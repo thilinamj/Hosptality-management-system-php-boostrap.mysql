b9741369f7943130e8c00e554bb3a7fea4db4ac6
Thilina001 <thilinamjayawardana@gmail.com>
2018-02-19 15:38:20 +0530

    create login page(second commit

---
+++ admin pannel.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" >
+</head>
+<body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
+</body>
+</html>
---
+++ func.php
@@ -0,0 +1,16 @@
+<?php
+$con=mysqli_connect("localhost","root","","hmsdb");
+if(isset($_POST['login_submit'])){
+$username=$_POST['username'];
+$password=$_POST['password'];
+$query="select * from logintb where username='$username' and password='$password'";
+$result=mysqli_query($con,$query);
+if(mysqli_num_rows($result)==1)
+{
+header("location:admin pannel.php");
+}
+else
+header("loaction");
+
+}
+?>
