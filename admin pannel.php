3fb129d803ed85c4498a9c69ee165eea3ff9213e
Thilina001 <thilinamjayawardana@gmail.com>
2018-02-26 23:56:52 +0530

    create more things od admin page

--- admin pannel.php
+++ admin pannel.php
@@ -6,18 +6,52 @@
 <body>
 
 <div class="jumbotron" style="background:url('images/loginbag.jpg')no-repeat;background-size:cover;height:300px;"></div>
-<div class="container">
+<div class="container-fluid">
 <div class="row">
-<div class="col-mid-5">
+<div class="col-md-3">
 <div class="list-group">
-<a href="" class="list-group-item active">Categories</a>
-<a href="" class="list-group-item ">Patient List</a>
-<a href="" class="list-group-item ">Patient List</a>
-<a href="" class="list-group-item ">Patient List</a>
+<a href="" class="list-group-item active">Patients</a>
+<a href="" class="list-group-item ">Patient Detailst</a>
+<a href="" class="list-group-item ">Add New Patient </a>
+<a href="" class="list-group-item ">Payment/Checkout</a>
+</div>
+<hr>
+<div class="list-group">
+<a href="" class="list-group-item active">Staff</a>
+<a href="" class="list-group-item ">Staff Details</a>
+<a href="" class="list-group-item ">Add New Staff</a>
+<a href="" class="list-group-item ">Remove Staff Members</a>
 </div>
 </div>
-<div class="col-mid-5"></div>
-<div class="col-mid-2"></div>
+
+<div class="col-md-8">
+<div class="card">
+<div class="card-body" style="background-color:#0080ff;color:#ffffff;">
+<h3>Book an Appontment</h3>
+</div> 
+<div class="card-body">
+<form class="form-group" action="func.php" method="post">
+<label>First Name:</label>
+<input type="text" name="fname" class="form-control"><br>
+<label>Last Name:</label>
+<input type="text" name="lname" class="form-control"><br>
+<label>Email id:</label>
+<input type="text" name="email" class="form-control"><br>
+<label>Contact:</label>
+<input type="text" name="contact" class="form-control"><br>
+<label>Doctor Appointment:</label>
+<select class="form-control">
+<option value="Dr Sharma from 6pm to 8pm">Dr Sharma from 6pm to 8pm</option>
+<option value="Dr Sharma from 6pm to 8pm">Dr Sharma from 6pm to 8pm</option>
+</select><br>
+<input type="submit" class="btn btn-primary" name="pat_submit" value"Enter Appointment"> 
+</form>
+</div>
+</div>
+</div> 
+<div class="col-md-1"></div>
+
+</div>
 </div>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
